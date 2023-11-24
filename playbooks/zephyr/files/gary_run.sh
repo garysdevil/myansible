178fc30a069c05074a855c8f2b152f0e75dca528
gary <adamgarysdevil@gmail.com>
2023-11-24 17:48:35 +0800

    --

--- playbooks/zephyr/files/gary_run.sh
+++ playbooks/zephyr/files/gary_run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-wallet=ZEPHXXX  # 钱包地址
+wallet=$1  # 钱包地址
 name="$(cat /proc/sys/kernel/hostname)" && if [ -n "$1" ]; then name="$1";fi
-pool=us-zephyr.miningocean.org:5452
+pool=us-zephyr.miningocean.org:5342 # Mid range hardware，没有SSL
 # proxy= # 海外SOCK5代理，否则连接矿池失败
 
 (grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
-&& ./xmrig -o ${pool} -u ${wallet} -p ${name} -a rx/0 -k --donate-level 1 --tls  -l ./xmrig_zephyr.log
+&& ./xmrig -o ${pool} -u ${wallet} -p ${name} -a rx/0 -k --donate-level 1 -l ./xmrig_zephyr.log
 # -x ${proxy}
+# --tls # 使用SSL连接

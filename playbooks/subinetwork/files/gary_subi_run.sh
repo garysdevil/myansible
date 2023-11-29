c583a26265eb52b300f3ea9dec0775ed87f62581
gary <adamgarysdevil@gmail.com>
2023-11-29 16:38:59 +0800

    update miner

--- playbooks/subinetwork/files/gary_subi_run.sh
+++ playbooks/subinetwork/files/gary_subi_run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-wallet=$1
-name="$(cat /proc/sys/kernel/hostname)" && if [ -n "$1" ]; then name="$1";fi
+wallet=钱包地址
+name="$(cat /proc/sys/kernel/hostname)" && if [ -n "$1" ]; then name=$(echo "$1" | sed 's/\./-/g');fi
 # proxy= SOCK5代理
 
 (grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
 && ./xmrig_subi -a ghostrider --url stratum-asia.rplant.xyz:17090 --tls --user ${wallet}.${name} -l ./xmrig_subi.log 
 # -x ${proxy}
+
+# 看日志 tail /opt/subi/xmrig_subi.log -n 100 | grep "10s/60s/15m"
--- playbooks/zephyr/files/gary_zeph_run.sh
+++ playbooks/zephyr/files/gary_zeph_run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-wallet=$1  # 钱包地址
+wallet=钱包地址
 name="$(cat /proc/sys/kernel/hostname)" && if [ -n "$1" ]; then name="$1";fi
 pool=us-zephyr.miningocean.org:5452
 # proxy= # SOCK5代理
 
 (grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
 && ./xmrig -o ${pool} -u ${wallet} -p ${name} -a rx/0 -k --donate-level 1 -l ./xmrig_zephyr.log --tls --cpu-max-threads-hint 90
-# -x ${proxy}
+# -x ${proxy} # SOCK5代理
 # --tls # 使用SSL连接
-# --cpu-max-threads-hint 90
+# --cpu-max-threads-hint 90 # 最大使用的线程数量，单位百分比
+
+# 看日志 tail /opt/zephyr/xmrig_zephyr.log -n 100 | grep "10s/60s/15m"

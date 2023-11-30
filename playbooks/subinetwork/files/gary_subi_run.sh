a9cd1f4454af95ef1f97d34e9e1bc2f3d93ea136
gary <adamgarysdevil@gmail.com>
2023-11-30 18:56:02 +0800

    20231130

--- playbooks/subinetwork/files/gary_subi_run.sh
+++ playbooks/subinetwork/files/gary_subi_run.sh
@@ -4,7 +4,7 @@ name="$(cat /proc/sys/kernel/hostname)" && if [ -n "$1" ]; then name=$(echo "$1"
 # proxy= SOCK5代理
 
 (grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
-&& ./xmrig_subi -a ghostrider --url stratum-asia.rplant.xyz:17090 --tls --user ${wallet}.${name} -l ./xmrig_subi.log 
+&& ./xmrig_subi -a ghostrider --url stratum-asia.rplant.xyz:17090 --tls --user ${wallet}.${name} --donate-level 1 -l ./xmrig_subi.log 
 # -x ${proxy}
 
 # 看日志 tail /opt/subi/xmrig_subi.log -n 100 | grep "10s/60s/15m"

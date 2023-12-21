53593cf05d7cc9a8b55d494ec700fa54e64b40cf
gary <adamgarysdevil@gmail.com>
2023-12-21 18:28:12 +0800

    20231221

--- playbooks/bellcoin/files/gary_bell_run.sh
+++ playbooks/bellcoin/files/gary_bell_run.sh
@@ -6,3 +6,6 @@ pool=stratum+tcp://yespower.jp1.mine.leywapool.com:6322
 proxy=socks5://SOCK5代理
 
 ./minerd -a scrypt -o ${pool} -u ${wallet} -p ${name} -x ${proxy}
+
+# pool=stratum+tcps://198.50.168.213:56234  # zpool 矿池
+# ./cpuminer -a yespower -u ${wallet} -p ${name},c=BELL,zap=BELL -o ${pool} -x  ${proxy}
